159621d454b778079045b0b8dbd025677b9d2b06
saad <saadgfx97@gmail.com>
2024-01-05 18:14:03 +0500

    payment method after amount

--- extensions/hide-cod-amount/src/run.js
+++ extensions/hide-cod-amount/src/run.js
@@ -17,9 +17,34 @@ const NO_CHANGES = {
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  const configuration = JSON.parse(
+  const CONFIGURED_AMOUNT = JSON.parse(
     input?.paymentCustomization?.metafield?.value ?? "{}"
   );
 
+  const cartValue = input.cart.cost.totalAmount.amount;
+
+  console.log(JSON.stringify({ CONFIGURED_AMOUNT }));
+
+  if (cartValue > CONFIGURED_AMOUNT) {
+    // Find the payment method to hide
+    const hidePaymentMethod = input.paymentMethods.find((method) =>
+      method.name.includes("COD")
+    );
+
+    if (!hidePaymentMethod) {
+      return NO_CHANGES;
+    }
+
+    return {
+      operations: [
+        {
+          hide: {
+            paymentMethodId: hidePaymentMethod.id,
+          },
+        },
+      ],
+    };
+  }
+
   return NO_CHANGES;
-};
+}
