ab833c07a4bb282116bb1bae3566b5c01aca8662
saad <saadgfx97@gmail.com>
2024-06-13 16:02:24 +0500

    hard coded the cities

---
+++ extensions/city-selector/src/Checkout.jsx
@@ -0,0 +1,71 @@
+import {
+  reactExtension,
+  Select,
+  useApplyShippingAddressChange,
+  useShippingAddress,
+} from '@shopify/ui-extensions-react/checkout';
+import { useEffect, useState } from 'react';
+
+export default reactExtension(
+  'purchase.checkout.block.render',
+  () => <Extension />,
+);
+
+function Extension() {
+  const address = useShippingAddress();
+  const [city, setCity] = useState('');
+  const updateShippingAddress = useApplyShippingAddressChange();
+  
+  const handleSelect = async (value) => {
+    try {
+      console.log(value, address)
+      setCity(value)
+      updateShippingAddress({
+        address: {
+          city: value
+        },
+        type: "updateShippingAddress"
+      })
+    } catch (error) {
+      console.log({error})
+    }
+  }
+  
+  const PAKISTAN_CITIES = "Karachi,Lahore,Kotla Qasim Khan,Faisalabad,Rawalpindi,Gujranwala,Peshawar,Multan,Hyderabad City,Islamabad,Quetta,Cantonment,Eminabad";
+  if (address?.countryCode == "PK") {
+    return (
+      <Select
+        label="Select city"
+        value={city}
+        onChange={(value) => handleSelect(value)}
+        options={
+          PAKISTAN_CITIES.split(',').map(city => ({
+            value: city,
+            label: city
+          }))
+        }
+      />
+    );
+  }
+  
+  const UK_CITIES = "London,Birmingham,Glasgow,Liverpool,Manchester,Sheffield,Leeds,Edinburgh,Bristol,Cardiff";
+  if (address?.countryCode == "GB") {
+    return (
+      <Select
+        label="Select city"
+        value={city}
+        onChange={(value) => handleSelect(value)}
+        options={
+          UK_CITIES.split(',').map(city => ({
+            value: city,
+            label: city
+          }))
+        }
+      />
+    );
+  }
+  
+
+
+  
+}
